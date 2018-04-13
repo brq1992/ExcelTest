29f4058faa518b19715a6bf937bdc12a45234a8a
admin <xbbhexbb@hotmail.com>
2018-04-13 16:50:48 +0800

    1

---
+++ WindowsFormsApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using NPOI.XWPF.UserModel;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace WindowsFormsApplication1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            System.Windows.Forms.Application.EnableVisualStyles();
+            System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+            System.Windows.Forms.Application.Run(new Form1());
+
+            XWPFDocument doc = new XWPFDocument();
+            doc.CreateParagraph();
+            using (FileStream sw = File.Create("blank.docx"))
+            {
+                doc.Write(sw);
+            }
+        }
+    }
+}
